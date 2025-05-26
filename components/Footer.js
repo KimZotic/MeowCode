7fc35bd07de2eb726c367dc9c2d02fd590f2637c
KimZotic <hakimzotic@gmail.com>
2025-05-26 19:19:22 +0800

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-dark border-t border-gray-800 text-center py-4 text-sm text-gray-400">
+      © 2025 MeowCode. Semua Hak Dilindungi.
+    </footer>
+  );
+}
